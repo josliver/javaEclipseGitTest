015d28f9f4ca4ee6904f929a26a7c4822e34fc9a
Jonathan Silva <jonathan.j.silva@gmail.com>
2016-03-16 11:27:07 -0300

    Se coloco el input

--- src/net/silva/examples/Main.java
+++ src/net/silva/examples/Main.java
@@ -1,9 +1,19 @@
 package net.silva.examples;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo para Git");
+		System.out.println("Dime un numero: ");
+		Scanner key = new Scanner(System.in);
+		
+		int numero = key.nextInt();
+		key.close();
+		System.out.println("Tu numero: " + Integer.toString(numero));
+		
+		
 		//This is a tests con mocos
 	}
 
